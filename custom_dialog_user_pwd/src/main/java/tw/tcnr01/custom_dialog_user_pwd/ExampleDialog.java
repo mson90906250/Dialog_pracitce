27404be2b4ae72c0055f2b1487b163bdbd1d3f81
mson90906250 <l90906250@gmail.com>
2019-02-07 11:44:36 +0800

    利用builder做一個dialog

--- custom_dialog_user_pwd/src/main/java/tw/tcnr01/custom_dialog_user_pwd/ExampleDialog.java
+++ custom_dialog_user_pwd/src/main/java/tw/tcnr01/custom_dialog_user_pwd/ExampleDialog.java
@@ -1,6 +1,46 @@
 package tw.tcnr01.custom_dialog_user_pwd;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatDialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
 
 public class ExampleDialog extends AppCompatDialogFragment {
+    private EditText editTextUsername;
+    private  EditText editTextPassword;
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        //return super.onCreateDialog(savedInstanceState);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        View view = inflater.inflate(R.layout.layout_dialog,null);
+
+        builder.setView(view)
+                .setTitle("Login")
+                .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    //什麽都不做就不用在裏頭打程式碼
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                })
+                .setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+
+        editTextUsername = view.findViewById(R.id.edit_username);
+        editTextPassword = view.findViewById(R.id.edit_password);
+
+        //回傳做好的dialog
+        return builder.create();
+    }
 }
