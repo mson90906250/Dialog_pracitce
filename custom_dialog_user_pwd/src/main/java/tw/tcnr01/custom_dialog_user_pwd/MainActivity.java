4dc5329977ad52f21a0fbe7138450ddca36b4c7b
mson90906250 <l90906250@gmail.com>
2019-02-07 11:19:22 +0800

    新增兩個textview 和 一個button 並給button設監聽

--- custom_dialog_user_pwd/src/main/java/tw/tcnr01/custom_dialog_user_pwd/MainActivity.java
+++ custom_dialog_user_pwd/src/main/java/tw/tcnr01/custom_dialog_user_pwd/MainActivity.java
@@ -2,12 +2,36 @@ package tw.tcnr01.custom_dialog_user_pwd;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView textViewUsername;
+    private TextView textViewPassword;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setupVIewComponent();
+    }
+
+    private void setupVIewComponent() {
+        textViewUsername = (TextView) findViewById(R.id.textview_username);
+        textViewPassword = (TextView) findViewById(R.id.textview_password);
+        button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
